53407daf28876a9c5e6cab565a2844fd33bad09f
Dustin Adams <dustinadams01@gmail.com>
2020-12-03 08:38:57 -0800

    cleaning up logic of day3 pt 2

--- 2020/day3/part2.py
+++ 2020/day3/part2.py
@@ -1,38 +1,32 @@
 """
 usage: $ python part2.py <input-file>
 """
-import sys
-
 import numpy as np
-
-print('currently not working')
-exit(1)
+import sys
 
 f = open(sys.argv[1], 'r')
 
-grid = [i.replace("\n", "") for i in f.readlines()]
-
-grid = np.array(grid)
+grid = np.array([i.replace("\n", "") for i in f.readlines()])
 
 slopes = ((1,1),(3,1),(5,1),(7,1),(1,2))
 
 product = 1
 for slope in slopes:
-
     collisions = 0
-
     dx=slope[0]
     dy=slope[1]
     x=0
     y=0
+
     while y < len(grid):
         if grid[y][x] == "#":
             collisions += 1
+
         x += dx
         x %= len(grid[y])
         y += dy
 
     product *= collisions
 
-print(collisions)
+print(product)
 
